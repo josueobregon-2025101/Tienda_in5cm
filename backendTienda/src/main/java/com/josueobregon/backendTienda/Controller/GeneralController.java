df3ce19331f0e5b40167f59e81e80c946116840c
josueobregon <jobregon-2025101@kinal.edu.gt>
2026-04-14 20:32:41 -0600

    Feat:se renombro la clase y se implementaron metodos

---
+++ backendTienda/src/main/java/com/josueobregon/backendTienda/Controller/GeneralController.java
@@ -0,0 +1,88 @@
+package com.josueobregon.backendTienda.Controller;
+
+import com.josueobregon.backendTienda.Entity.Usuarios;
+import com.josueobregon.backendTienda.Service.*;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GeneralController {
+    @Autowired
+    private UsuariosService usuariosService;
+    @Autowired
+    private ClientesService clientesService;
+    @Autowired
+    private ProductosService productosService;
+    @Autowired
+    private VentasService ventasService;
+    @Autowired
+    private DetalleVentaService detalleVentaService;
+
+    //Index
+
+    @GetMapping("/Index")
+    public String index(HttpSession session,Model model) {
+            Usuarios usuario = (Usuarios) session.getAttribute("usuarioLogueado");
+            model.addAttribute("usuario", usuario);
+
+        model.addAttribute("usuarios", usuariosService.findAll());
+        model.addAttribute("clientes", clientesService.findAll());
+        model.addAttribute("productos", productosService.findAll());
+        model.addAttribute("ventas", ventasService.findAll());
+        model.addAttribute("detalle",detalleVentaService.obtenerDetalle());
+        return "Pages/Index";
+    }
+    @GetMapping("/eliminar/usuarios/{id}")
+    public String eliminarU(@PathVariable int id) {
+        usuariosService.DeleteUsuario(id);
+        return "redirect:/Index";
+    }
+
+    @GetMapping("/eliminar/ventas/{id}")
+    public String eliminarV(@PathVariable int id) {
+        ventasService.DeleteVenta(id);
+        return "redirect:/Index";
+    }
+
+    @GetMapping("/eliminar/clientes/{id}")
+    public String eliminarC(@PathVariable int id) {
+        clientesService.DeleteCliente(id);
+        return "redirect:/Index";
+    }
+
+    @GetMapping("/eliminar/productos/{id}")
+    public String eliminarP(@PathVariable int id) {
+        productosService.DeleteProducto(id);
+        return "redirect:/Index";
+    }
+    @GetMapping("/eliminar/detalle/{id}")
+    public String eliminarD(@PathVariable int id) {
+        detalleVentaService.deleteDetalle(id);
+        return "redirect:/Index";
+    }
+
+    //login
+    @GetMapping("/log")
+    public String Login() {return "Pages/login";}
+
+    @PostMapping("/login")
+    public String validar(@RequestParam String usuario, @RequestParam String password,
+                        HttpSession session,Model model) {
+        Usuarios u = usuariosService.login(usuario,password);
+
+        if (u != null) {
+            session.setAttribute("usuarioLogueado", u);
+            return "redirect:/Index";
+        } else {
+            model.addAttribute("error", "Usuario y contraseña incorrecta");
+            return "Pages/login";
+        }
+    }
+}
+
